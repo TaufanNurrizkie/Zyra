2e196419d9110c0b0e1c10948bb80f392c9ed2b5
Muhamad Jalaludin Rumi <muhammadrumi876@gmail.com>
2025-08-18 14:26:30 +0700

    Kontak

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::get('/informasi', function () {
     return Inertia::render('user/informasi/index');
 })->middleware(['auth', 'verified'])->name('informasi');
 
+Route::get('/kontak', function () {
+    return Inertia::render('user/kontak/index');
+})->name('user.kontak');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
